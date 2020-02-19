964166f9917d73be29ff2b265c58174629474217
george0131 <jorgeibanez7@gmail.com>
2020-02-19 17:50:01 -0500

    vinculación de aliados y usuarios

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -6,33 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
-name :string;
-lastname :string;
-celular : any;
-cedula : any;
-correo: string;
-fecha : any;
-estadocivil : any;
+  name: string;
+  lastname: string;
+  celular: any;
+  cedula: any;
+  correo: string;
+  fecha: any;
+  estadocivil: any;
 
   constructor() { }
 
   ngOnInit() {
-    
-    this.name=localStorage.getItem('name') + " " + localStorage.getItem('lastName');
-    this.correo=localStorage.getItem('email');
-    this.cedula=localStorage.getItem('cedula');
-    this.celular=localStorage.getItem('celular');  
+
+    this.name = localStorage.getItem('name') + ' ' + localStorage.getItem('lastName');
+    this.correo = localStorage.getItem('email');
+    this.cedula = localStorage.getItem('cedula');
+    this.celular = localStorage.getItem('celular');
   }
 
-registrar(){
-  let datos={
-    "names":this.name,
-    "lastNames":this.lastname,
-    "cellPhone": this.celular.toString(),
-    "documentId":this.cedula.toString(),
-    "email":this.email,
-    "password":hash
-  }
+  registrar() {
+    /*let datos = {
+      "names": this.name,
+      "lastNames": this.lastname,
+      "cellPhone": this.celular.toString(),
+      "documentId": this.cedula.toString(),
+      "email": this.email,
+      "password": hash
+    }*/
 
-}
+  }
 }
